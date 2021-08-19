17540fd332a2e08c7f43b1560a968ffcccf6ecc0
unknown <arake72@naver.com>
2021-08-19 14:22:43 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      create-next-app
+    </div>
+  )
+}
+
+
+//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=ajdkfl6445&logNo=221409821565
+
+//https://www.youtube.com/watch?v=Ujjdn2wMIew&list=PLZKTXPmaJk8Lx3TqPlcEAzTL8zcpBz7NP&ab_channel=%EC%BD%94%EB%94%A9%EC%95%99%EB%A7%88
+
+//https://github.com/
+
